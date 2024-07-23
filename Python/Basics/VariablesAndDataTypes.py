7cd26d1ef76e1eec1c8eecf82885f7b1c082a57d
Tharudi-Perera <115540243+Tharudi-Perera@users.noreply.github.com>
2024-07-23 15:31:06 +0530

    DataTypes

---
+++ Python/Basics/VariablesAndDataTypes.py
@@ -0,0 +1,9 @@
+#Variables in Python are created when first assigned. 
+#Python supports several data types including integers, floats, strings, and booleans.
+
+name = "Alice"          # String
+age = 30                # Integer
+height = 5.6            # Float
+is_student = True       # Boolean
+
+print("name =" ,name, ",age =",age, ",height =",height, ",student or not =" ,is_student)
